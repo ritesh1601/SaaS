d37fff3f801c7256ab8a3f1c4e59e6bea324e120
ritesh <riteshsaini1601@gmail.com>
2025-01-04 14:08:31 +0530

    most Working

--- NewP/NewP/src/main.jsx
+++ NewP/NewP/src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {Provider} from 'react-redux';
-import {createStore , applyMiddleware ,componse} from 'react-redux';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
